8b1049e14021d80b80461a43cdc1ef87cc08069b
jobc90 <jobc90@gmail.com>
2023-03-24 11:11:22 +0900

    router_start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function Test1() {
+  return (<h1>Test1</h1>);
+}
+
+function Test2() {
+  return (<h1>Test2</h1>);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/t1" Component={Test1}/>
+        <Route path="/t2" Component={Test2}/>
+      </Routes>
+    </>
   );
 }
 
