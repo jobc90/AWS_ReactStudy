035a53d206342bc071840ab8f183c858cb0f2f96
jobc90 <jobc90@gmail.com>
2023-03-24 11:15:34 +0900

    1115commit

---
+++ src/pages/InputSample/InputSample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InputSample = () => {
+    const [text, setText] = useState('');
+    return (
+        <div>
+            <input type="text" onChange={} value={text}/>
+        </div>
+    );
+};
+
+export default InputSample;
